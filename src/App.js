3ba9f50856521508c20d7dcad1c16207046131c9
kunal-develope <kunalkk45451@gmail.com>
2023-05-23 12:25:09 +0530

    added search button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./App.css";
-import Header from "./components/header/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./components/main/main";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NotFound from "./components/notfound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/main/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
