c211d1eed1418a306def8ca213453364f61fd8c8
VIPIN KUMAR <vipinrajandcompany@gmail.com>
2023-05-23 16:58:03 +0530

    card added

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import MovieCard from "../card/movieCard";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./main.css";
 
 const Main = () => {
+  const movieList = [
+    "movie1",
+    "movie2",
+    "movie3",
+    "movie4",
+    "movie5",
+    "movie6",
+    "movie7",
+  ];
   return (
     <div>
       <Header />
-      <Footer />
+      <Container>
+        <Row>
+          {movieList.map((movie) => {
+            return (
+              <Col className="Card-col">
+                <MovieCard movieDetails={movie} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 };
