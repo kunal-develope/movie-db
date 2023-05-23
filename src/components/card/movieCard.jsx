62db7be019babf4152d92e3cec88e5bb797a336b
kunal-develope <kunalkk45451@gmail.com>
2023-05-23 18:03:40 +0530

    added list,api calls ,image update

--- src/components/card/movieCard.jsx
+++ src/components/card/movieCard.jsx
@@ -6,14 +6,16 @@ const MovieCard = ({ movieDetails }) => {
   return (
     <div>
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img
+          variant="top"
+          src={
+            "https://image.tmdb.org/t/p/original" + movieDetails?.poster_path
+          }
+        />
         <Card.Body>
-          <Card.Title>{movieDetails}</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Card.Title>{movieDetails?.original_title}</Card.Title>
+          <Card.Text>{movieDetails?.vote_average}</Card.Text>
+          <Button variant="primary">{movieDetails.release_date}</Button>
         </Card.Body>
       </Card>
     </div>
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import MovieCard from "../card/movieCard";
@@ -8,15 +8,33 @@ import Col from "react-bootstrap/Col";
 import "./main.css";
 
 const Main = () => {
-  const movieList = [
-    "movie1",
-    "movie2",
-    "movie3",
-    "movie4",
-    "movie5",
-    "movie6",
-    "movie7",
-  ];
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    if (!movieList.length) getData();
+  }, [movieList]);
+
+  const getData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTRhMjRhNWM0NjEyOTRjZWJiZGFlYTcxOGY3NWVkOSIsInN1YiI6IjY0NmNhODQ3ZDE4NTcyMDE2MTkzNTE1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Zi0v-Ch742Axssum3MvZ27GTx6hUY6SA55GrJXYRd88",
+      },
+    };
+    await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=da4a24a5c461294cebbdaea718f75ed9",
+      options
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setMovieList(json?.results);
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
     <div>
       <Header />
